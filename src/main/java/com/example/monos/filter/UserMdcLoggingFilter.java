40adc5162e19fe622a28e230a9e1d4871fa6c417
elleryq1125 <86918125+elleryq1125@users.noreply.github.com>
2025-10-21 23:54:06 +0900

    add/ログ出力を作成
    slf4jを利用し、ログ出力の基盤を整え、以下のフィルタを作成。
    ・MDCに認証済のユーザIDを格納するフィルタ
    ・共通のアクセス情報をログに記録するフィルタ

---
+++ src/main/java/com/example/monos/filter/UserMdcLoggingFilter.java
@@ -0,0 +1,41 @@
+package com.example.monos.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+/**
+ * <p>MDCにログで利用する認証情報を格納する。</p>
+ * @author t.ueta
+ */
+public class UserMdcLoggingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) 
+    		throws ServletException, IOException {
+
+        try {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            if (auth != null && auth.isAuthenticated() 
+            		&& !"anonymousUser".equals(auth.getPrincipal())) {
+            	// ユーザ名をMDCに格納
+                MDC.put("user", auth.getName()); 
+            } else {
+            	// 未ログイン時
+                MDC.put("user", "anonymous");    
+            }
+
+            filterChain.doFilter(request, response);
+        } finally {
+        	// スレッド再利用対策（必ず削除）
+            MDC.remove("user"); 
+        }
+    }
+}
