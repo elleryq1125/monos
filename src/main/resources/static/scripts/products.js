209f65e973a81e162603070c94a4d7ca27352045
elleryq1125 <86918125+elleryq1125@users.noreply.github.com>
2026-03-11 21:54:13 +0900

    update/商品の追加・更新モーダル作成途中

---
+++ src/main/resources/static/scripts/products.js
@@ -0,0 +1,8 @@
+function openProductAddModal(){
+
+  document.getElementById("modalTitle").innerText = "商品登録"
+  document.getElementById("productId").value = ""
+  document.getElementById("productName").value = ""
+  document.getElementById("price").value = ""
+
+}
