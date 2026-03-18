8dfee8db4d972e3fe68f54ff19cefabaacfb3175
elleryq1125 <86918125+elleryq1125@users.noreply.github.com>
2026-03-18 23:10:27 +0900

    add/API用の共通例外クラスを作成

---
+++ src/main/java/com/example/monos/exception/ApiGrobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.example.monos.exception;
+
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.example.monos.dto.ApiResponse;
+
+/**
+ * API用の共通例外クラス
+ * @author t.ueta
+ */
+@RestControllerAdvice(annotations = RestController.class)
+public class ApiGrobalExceptionHandler {
+	private final MessageSource messageSource;
+	
+	public ApiGrobalExceptionHandler(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+	
+	@ExceptionHandler(BusinessException.class)
+	public ApiResponse<?> handleBusiness(BusinessException e){
+		return ApiResponse.validationError(e.getErrors());
+	}
+	
+	@ExceptionHandler(FatalBusinessException.class)
+	public ApiResponse<?> handleFatalBusiness(FatalBusinessException e){
+		return ApiResponse.errorMessage(e.getMessage());
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ApiResponse<?> handle(Exception e){
+		return ApiResponse.errorMessage(messageSource.getMessage("systemException", null, Locale.JAPAN));
+	}
+}
