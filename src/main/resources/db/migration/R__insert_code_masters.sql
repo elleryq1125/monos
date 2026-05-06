1905e78d60a2db601f6bebafd72ec4d0bb520689
elleryq1125 <86918125+elleryq1125@users.noreply.github.com>
2026-05-06 16:31:25 +0900

    add/コードマスタ「入庫予定ステータス」 初期値設定ファイル追加
    Rファイルは削除。

--- src/main/resources/db/migration/R__insert_code_masters.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO code_masters(code_type, code, name, display_order) VALUES ('INBOUND_STATUS', 0, '未入庫', 1);
-INSERT INTO code_masters(code_type, code, name, display_order) VALUES ('INBOUND_STATUS', 1, '入庫中', 2);
-INSERT INTO code_masters(code_type, code, name, display_order) VALUES ('INBOUND_STATUS', 2, '入庫済', 3);
