7f671ee5626ae9114007a813342b96dd671a9e14
elleryq1125 <86918125+elleryq1125@users.noreply.github.com>
2026-05-20 19:48:54 +0900

    update/入庫実績テーブル 商品ID・倉庫IDを削除

--- src/main/resources/db/migration/V1.5__create_inbound_tables.sql
+++ src/main/resources/db/migration/V1.5__create_inbound_tables.sql
@@ -19,8 +19,6 @@ CREATE TABLE inbound_schedules (
 CREATE TABLE inbound_results (
 	inbound_result_id SERIAL PRIMARY KEY,
 	company_id INTEGER  NOT NULL,
-	product_id INTEGER  NOT NULL,
-	warehouse_id INTEGER  NOT NULL,
 	inbound_schedule_id INTEGER  NOT NULL,
 	result_qty INTEGER  NOT NULL,
 	result_date DATE  NOT NULL,
@@ -29,7 +27,5 @@ CREATE TABLE inbound_results (
 	deleted_at TIMESTAMP WITH TIME ZONE,
 	
 	CONSTRAINT fk_inbound_results_company_id FOREIGN KEY (company_id) REFERENCES companies (company_id) ON DELETE RESTRICT,
-	CONSTRAINT fk_inbound_results_product_id FOREIGN KEY (product_id) REFERENCES products (product_id) ON DELETE RESTRICT,
-	CONSTRAINT fk_inbound_results_warehouse_id FOREIGN KEY (warehouse_id) REFERENCES warehouses (warehouse_id) ON DELETE RESTRICT,
 	CONSTRAINT fk_inbound_results_inbound_schedule_id FOREIGN KEY (inbound_schedule_id) REFERENCES inbound_schedules (inbound_schedule_id) ON DELETE RESTRICT
 );
