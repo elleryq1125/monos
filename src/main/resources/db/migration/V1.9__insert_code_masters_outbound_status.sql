f06775235a1209b82c59bb7eb8e670b7364000f1
elleryq1125 <86918125+elleryq1125@users.noreply.github.com>
2026-06-04 21:24:18 +0900

    add/コードマスタ 出庫ステータス追加

---
+++ src/main/resources/db/migration/V1.9__insert_code_masters_outbound_status.sql
@@ -0,0 +1,4 @@
+INSERT INTO code_masters(code_type, code, name, display_order) VALUES ('OUTBOUND_STATUS', 0, '未出庫', 1);
+INSERT INTO code_masters(code_type, code, name, display_order) VALUES ('OUTBOUND_STATUS', 1, '出庫中', 2);
+INSERT INTO code_masters(code_type, code, name, display_order) VALUES ('OUTBOUND_STATUS', 2, '出庫済', 3);
+INSERT INTO code_masters(code_type, code, name, display_order) VALUES ('OUTBOUND_STATUS', 3, 'キャンセル', 4);
