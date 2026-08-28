43f16611eb488714b8da13384c848aba1fb418b7
elleryq1125 <86918125+elleryq1125@users.noreply.github.com>
2026-08-29 00:34:20 +0900

    update/在庫テーブルのALTER文を修正し、カラム追加を個別に記述

--- src/main/resources/db/migration/V1.11__alter_inventories_add_stock_settings.sql
+++ src/main/resources/db/migration/V1.11__alter_inventories_add_stock_settings.sql
@@ -1,3 +1,2 @@
-ALTER TABLE inventories
-    ADD COLUMN reorder_point INTEGER NOT NULL DEFAULT 0,
-    ADD COLUMN appropriate_stock_qty INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE inventories ADD COLUMN reorder_point INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE inventories ADD COLUMN appropriate_stock_qty INTEGER NOT NULL DEFAULT 0;
